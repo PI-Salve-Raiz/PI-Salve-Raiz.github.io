64e1320b0bf533de082aa619c1c0a90df6cc942e
Julia Bernardes <129619510+JuliaBernardess@users.noreply.github.com>
2023-08-01 10:32:44 -0300

    Task 8

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+
+import './App.css';
+import Home from './pages/home/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/login/Login';
+import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
+import Sobre from './pages/sobre/Sobre';
+
+function App() {
+  return (
+    <>
+   
+        <BrowserRouter>
+          <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/sobre" element={<Sobre />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+  
+    </>
+  );
+}
+export default App;
---
+++ src/pages/sobre/Sobre.tsx
@@ -0,0 +1,13 @@
+function Sobre() {
+    return (
+        <>
+            <div className="h-96 mb-96">
+
+                <h1 className='text-xl'>Sobre</h1>
+
+            </div>
+        </>
+    );
+}
+
+export default Sobre;
