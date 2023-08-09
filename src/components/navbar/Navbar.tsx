1310766bb25aece5be3712f38112442c7124799f
augustofeitosa <feitosa66@gmail.com>
2023-08-09 17:06:29 -0300

    atualização

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -18,7 +18,7 @@ function Navbar() {
 
     if(usuario.token !== "") {
       navbarComponent = (
-        <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
+        <div className='w-full bg-gradient-to-r from-cyan-600 to-blue-600 text-white flex justify-center py-4'>
           <div className="container flex justify-between text-lg">
             <Link to='/home' className='text-2xl font-bold uppercase'>Salve Raiz</Link>
 
