13711eba32d3e8b894c40afa0527d85ee9215801
Julia Bernardes <129619510+JuliaBernardess@users.noreply.github.com>
2023-08-09 23:08:09 -0300

    Task 12 finais

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -9,7 +9,7 @@ function Home() {
           <div className='container grid grid-cols-2 text-white'>
             <div className="flex flex-col gap-4 items-center justify-center py-4">
               <h2 className='text-5xl font-bold  text-white'>Seja bem vinde!</h2>
-              <p className='text-xl  text-white'>Expresse aqui seus pensamentos e opniões</p>
+              <p className='text-xl  text-white'>Construindo pontes de apoio e oportunidade dentro da nossa própria comunidade.</p>
 
               <div className="flex justify-around gap-4">
               <ModalPostagem />
