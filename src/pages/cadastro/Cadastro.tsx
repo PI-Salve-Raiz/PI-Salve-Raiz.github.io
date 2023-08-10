1de8de598f9928ec2bdd1a67e63bf55e05dc22d6
augustofeitosa <feitosa66@gmail.com>
2023-08-10 00:23:40 -0300

    @AugustoFeitosa Ajuste estilização
    Login e Cadastro

--- src/pages/cadastro/Cadastro.tsx
+++ src/pages/cadastro/Cadastro.tsx
@@ -144,7 +144,7 @@ function Cadastro() {
             <button className='rounded text-white bg-red-400 hover:bg-red-700 w-1/2 py-2' onClick={back}>
               Cancelar
             </button>
-            <button className='rounded text-white bg-indigo-400 hover:bg-indigo-900 w-1/2 py-2' type='submit'>
+            <button className='rounded text-white bg-indigo-500 hover:bg-indigo-400 focus:bg-indigo-400 w-1/2 py-2' type='submit'>
               Cadastrar
             </button>
           </div>
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -12,88 +12,122 @@ function Login() {
   const navigate = useNavigate();
 
   const [usuarioLogin, setUsuarioLogin] = useState<UsuarioLogin>(
-    {} as UsuarioLogin
+    {} as UsuarioLogin,
   );
 
   const { usuario, handleLogin } = useContext(AuthContext);
 
-  const {isLoading} = useContext(AuthContext) 
+  const { isLoading } = useContext(AuthContext);
 
   useEffect(() => {
-    if (usuario.token !== "") {
-        navigate('/home')
+    if (usuario.token !== '') {
+      navigate('/home');
     }
-}, [navigate, usuario])
+  }, [navigate, usuario]);
 
-function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
-  setUsuarioLogin({
+  function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
+    setUsuarioLogin({
       ...usuarioLogin,
-      [e.target.name]: e.target.value
-  })
-} 
+      [e.target.name]: e.target.value,
+    });
+  }
 
-function login(e: ChangeEvent<HTMLFormElement>) {
-  e.preventDefault()
-  handleLogin(usuarioLogin)
-}
+  function login(e: ChangeEvent<HTMLFormElement>) {
+    e.preventDefault()
+    handleLogin(usuarioLogin)
+  }
   return (
     <>
       <section className="flex flex-col md:flex-row h-screen items-center">
+        <div className="bg-indigo-600 hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
+          <Carrossel />
+        </div>
 
-<div className="bg-indigo-600 hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
-  <Carrossel/>
-</div>
+        <div
+          className="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12
+      flex items-center justify-center"
+        >
+          <div className="w-full h-100">
+            <h1 className="text-xl md:text-3xl font-bold leading-tight mt-12">
+              Faça login na sua conta
+            </h1>
 
-<div className="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12
-      flex items-center justify-center">
+            <form onSubmit={login}>
+              <div>
+                <label className="block text-gray-700">Endereço de email</label>
+                <input
+                  type="email"
+                  name="usuario"
+                  id="usuario"
+                  placeholder="Endereço de email"
+                  className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none"
+                  autoFocus
+                  required
+                  value={usuarioLogin.usuario}
+                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                    atualizarEstado(e)
+                  }
+                />
+              </div>
 
-  <div className="w-full h-100">
+              <div className="mt-4">
+                <label className="block text-gray-700">Senha</label>
+                <input
+                  type="password"
+                  name="senha"
+                  id="senha"
+                  placeholder="Senha"
+                  minLength="6"
+                  className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500
+              focus:bg-white focus:outline-none"
+                  required
+                  value={usuarioLogin.senha}
+                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                    atualizarEstado(e)
+                  }
+                />
+              </div>
 
+              <div className="text-right mt-2">
+                <a
+                  href="#"
+                  className="text-sm font-semibold text-gray-700 hover:text-blue-700 focus:text-blue-700"
+                >
+                  Esqueceu sua senha?
+                </a>
+              </div>
 
-    <h1 className="text-xl md:text-3xl font-bold leading-tight mt-12">Faça login na sua conta</h1>
+              <button
+                type="submit"
+                className="w-full block bg-indigo-500 hover:bg-indigo-400 focus:bg-indigo-400 text-white font-semibold rounded-lg
+            px-4 py-3 mt-6" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+              >
+                {isLoading ? (
+                  <RotatingLines
+                    strokeColor="white"
+                    strokeWidth="5"
+                    animationDuration="0.75"
+                    width="24"
+                    visible={true}
+                  />
+                ) : (
+                  <span>Entrar</span>
+                )}
+              </button>
+            </form>
 
-    <form className="mt-6" action="#" method="POST">
-      <div>
-        <label className="block text-gray-700">Endereço de email</label>
-        <input type="email" name="" id="" placeholder="Endereço de email" className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autoFocus required value={usuarioLogin.usuario} 
-              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}/>
-      </div>
+            <hr className="my-6 border-gray-300 w-full"></hr>
 
-      <div className="mt-4">
-        <label className="block text-gray-700">Senha</label>
-        <input type="password" name="" id="" placeholder="Senha" minlength="6" className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500
-              focus:bg-white focus:outline-none" required  value={usuarioLogin.senha} 
-              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}/>
-      </div>
-
-      <div className="text-right mt-2">
-        <a href="#" className="text-sm font-semibold text-gray-700 hover:text-blue-700 focus:text-blue-700">Esqueceu sua senha?</a>
-      </div>
-
-      <button type="submit" className="w-full block bg-indigo-500 hover:bg-indigo-400 focus:bg-indigo-400 text-white font-semibold rounded-lg
-            px-4 py-3 mt-6">{isLoading ? <RotatingLines
-              strokeColor="white"
-              strokeWidth="5"
-              animationDuration="0.75"
-              width="24"
-              visible={true}
-            /> :
-              <span>Entrar</span>}</button>
-    </form>
-
-    <hr className="my-6 border-gray-300 w-full"></hr>
-
-    <p className="mt-5"> Ainda não tem uma conta?{' '}
-            <Link to="/cadastro" className="text-indigo-800 hover:underline">
-              Cadastre-se
-            </Link></p>
-
-
-  </div>
-</div>
-
-</section>
-
+            <p className="mt-5">
+              {' '}
+              Ainda não tem uma conta?{' '}
+              <Link to="/cadastro" className="text-indigo-800 hover:underline">
+                Cadastre-se
+              </Link>
+            </p>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
